b0d4e086e4b80dc4ce22e0d439cb7c76944acd99
axelcartaya <axelcartaya1@gmail.com>
2026-04-01 15:40:36 +0200

    feat: implementación migraciones

--- Domain/Models/ApplicationUser.cs
+++ Domain/Models/ApplicationUser.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Domain.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(50)]
+        public string? Alias { get; set; }
         public virtual ICollection<Acuario> Acuarios { get; set; } = new List<Acuario>();
     }
 }
